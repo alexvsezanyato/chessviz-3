9b961a42aba5e672acbb0ec38bb083bfaec526a8
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2020-04-19 20:08:58 +0700

    Add the ability to select the input file name

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <fstream>
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::ifstream file("source/input");
+	std::ifstream file;
+
+	if (argc == 2) file.open(argv[1]);
+	else 
+	{
+		std::cout << "Wrong input. Type 'chessviz --help' to get help \n";
+		return 1;
+	}
 
 	if (!file) 
 	{
-		std::cout << "Fail to open source/input \n";
+		std::cout << "Fail to open " << argv[1] << "\n";
 		return 1;
 	}
 }
